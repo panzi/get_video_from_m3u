9beb44d575aa927e3a176d351299a7216b1fa385
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2015-08-23 18:13:36 +0200

    use python dbus bindings and make download nicely cancellable

--- get_video_from_m3u.py
+++ get_video_from_m3u.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
+# coding: UTF-8
 
-from __future__ import print_function
+from __future__ import print_function, division
 
 import re
 import os
@@ -8,13 +9,23 @@ import subprocess
 import shlex
 import traceback
 import requests
+import dbus
+from time import time
 from lxml import html
 from urlparse import urljoin, urlparse
-from contextlib import closing
+from contextlib import closing, contextmanager
 
 RE_EXT = re.compile('\.[a-z]*$',re.I)
 CAPTION = 'Get Video from M3U'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
 DROP_HEADERS = {'if-none-match', 'if-modified-since', 'accept-encoding', 'upgrade-insecure-requests', 'connection'}
+
+def fmt_span(seconds):
+	minutes  = seconds // 60
+	seconds -= minutes * 60
+	hours    = minutes // 60
+	minutes -= hours * 60
+	return "%02d:%02d:%02d" % (hours, minutes, seconds)
 
 def text_cmd(*cmd):
 	p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
@@ -30,7 +41,7 @@ def bool_cmd(*cmd):
 	status = p.wait()
 	if status == 0:
 		return True
-	elif status == 2:
+	elif status == 1:
 		return False
 	else:
 		raise KeyboardInterrupt
@@ -73,19 +84,26 @@ def passive_popup(text,timeout=5):
 def show_error(text):
 	subprocess.check_call(['kdialog','--error',text,'--caption',CAPTION])
 
+@contextmanager
+def progressbar(text,maximum):
+	bus_name, object_path = text_cmd('kdialog','--progressbar',text,str(maximum),'--caption',CAPTION).split()
+	bus = dbus.SessionBus()
+	bar = bus.get_object(bus_name, object_path)
+	try:
+		yield bar
+	finally:
+		bar.close()
+
 class Progressbar(object):
 	def __init__(self,text,maximum):
 		self._value  = 0
-		self._handle = text_cmd('kdialog','--progressbar',text,str(maximum),'--caption',CAPTION).split()
+		bus_name, object_path = text_cmd('kdialog','--progressbar',text,str(maximum),'--caption',CAPTION).split()
 
 	def step(self):
 		self.value = self._value + 1
 
 	def _get_value(self):
-		value = int(check_call_errmsg(['qdbus',self._handle[0], self._handle[1], 'org.kde.kdialog.ProgressDialog.value'],
-			stdout=subprocess.PIPE).strip())
-		self._value = value
-		return value
+		return self._value
 
 	def _set_value(self,value):
 		check_call_errmsg(['qdbus',self._handle[0], self._handle[1], 'org.kde.kdialog.ProgressDialog.value', str(value)])
@@ -103,96 +121,129 @@ class Progressbar(object):
 		self.close()
 
 def get_video_from_m3u(curl=None,outfile=None):
-	if curl is None:
-		curl = inputbox('Paste M3U URL/cURL from network tab:')
-	headers = {}
-	if not curl.startswith('curl '):
-		m3u_url = curl
-		headers['user-agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
-	else:
-		m3u_url = None
-		it = iter(shlex.split(curl))
-		next(it)
-		for arg in it:
-			if arg == '-H':
-				key, value = next(it).split(':',1)
-				key = key.lower()
-				if key not in DROP_HEADERS:
-					headers[key] = value.strip()
+	try:
+		if curl is None:
+			curl = inputbox('Paste M3U URL/cURL from network tab:')
+		headers = {}
+		if not curl.startswith('curl '):
+			m3u_url = curl
+			headers['user-agent'] = USER_AGENT
+		else:
+			m3u_url = None
+			it = iter(shlex.split(curl))
+			next(it)
+			for arg in it:
+				if arg == '-H':
+					key, value = next(it).split(':',1)
+					key = key.lower()
+					if key not in DROP_HEADERS:
+						headers[key] = value.strip()
 
-			elif arg == '--compressed':
-				pass
+				elif arg == '--compressed':
+					pass
 
-			elif arg.startswith('-'):
-				raise ValueError('Cannot parse cURL command line because of unknown argument: '+arg)
+				elif arg.startswith('-'):
+					raise ValueError('Cannot parse cURL command line because of unknown argument: '+arg)
 
-			elif m3u_url is not None:
-				raise ValueError('Cannot parse cURL command line because it contains more than one url:\n%s\n%s' % (m3u_url, arg))
+				elif m3u_url is not None:
+					raise ValueError('Cannot parse cURL command line because it contains more than one url:\n%s\n%s' % (m3u_url, arg))
 
-			else:
-				m3u_url = arg
+				else:
+					m3u_url = arg
 
-	if outfile is None:
-		outfile = get_save_filename(filter='*.ts')
+		if outfile is None:
+			outfile = get_save_filename(filter='*.ts')
 
-	with requests.session() as session:
-		resp = session.get(m3u_url, headers=headers)
-		resp.raise_for_status()
-		data = resp.text
+		outname = os.path.split(outfile)[1]
 
-		content_type = resp.headers['content-type'].split(";")[0]
-		if content_type == 'text/html':
-			# it was html, lets try to resolve crappy refresh redirect like t.co uses
-			doc = html.fromstring(data)
-			meta = doc.cssselect("meta[http-equiv='refresh']")
-			if meta:
-				params = {}
-				for param in meta[0].attrib['content'].split(";")[1:]:
-					key, val = param.split('=',1)
-					params[key.lower()] = val
+		with requests.session() as session:
+			with progressbar('Downloading »%s« ETA ---:--:--' % outname,1) as progress:
+				progress.showCancelButton(True)
 
-				m3u_url = params['url']
 				resp = session.get(m3u_url, headers=headers)
 				resp.raise_for_status()
 				data = resp.text
 
-		if resp.url.startswith('https://www.periscope.tv/w/'):
-			# it was a periscope video page (html) instead
-			doc = html.fromstring(data)
-			meta = doc.cssselect("meta[property='og:image']")
+				if progress.wasCancelled():
+					raise KeyboardInterrupt
 
-			if not meta:
-				raise Exception("could not find video info in referred page")
+				content_type = resp.headers['content-type'].split(";")[0]
+				if content_type == 'text/html':
+					# it was html, lets try to resolve crappy refresh redirect like t.co uses
+					doc = html.fromstring(data)
+					meta = doc.cssselect("meta[http-equiv='refresh']")
+					if meta:
+						params = {}
+						for param in meta[0].attrib['content'].split(";")[1:]:
+							key, val = param.split('=',1)
+							params[key.lower()] = val
 
-			image_url = urlparse(meta[0].attrib['content'])
-			code      = RE_EXT.sub('', image_url.path.split("/")[2])
-			m3u_url   = 'https://replay.periscope.tv/%s/playlist.m3u8' % code
-
-			resp = session.get(m3u_url, headers=headers)
-			resp.raise_for_status()
-			data = resp.text
-
-		chunk_urls = [urljoin(m3u_url, line) for line in data.split('\n') if line and line[0] != '#']
-
-		with Progressbar('Downloading %s:' % os.path.split(outfile)[1],len(chunk_urls)) as progress:
-			with open(outfile,'wb') as fp:
-				for chunk_url in chunk_urls:
-					print('downloading:',chunk_url)
-					with closing(session.get(chunk_url, headers=headers, stream=True)) as resp:
+						m3u_url = params['url']
+						resp = session.get(m3u_url, headers=headers)
 						resp.raise_for_status()
-						for data in resp.iter_content(8192):
-							fp.write(data)
-					progress.step()
+						data = resp.text
 
-	passive_popup('Filished saving video: '+outfile)
+						if progress.wasCancelled():
+							raise KeyboardInterrupt
+
+				if resp.url.startswith('https://www.periscope.tv/w/'):
+					# it was a periscope video page (html) instead
+					doc = html.fromstring(data)
+					meta = doc.cssselect("meta[property='og:image']")
+
+					if not meta:
+						raise Exception("could not find video info in referred page")
+
+					image_url = urlparse(meta[0].attrib['content'])
+					code      = RE_EXT.sub('', image_url.path.split("/")[2])
+					m3u_url   = 'https://replay.periscope.tv/%s/playlist.m3u8' % code
+
+					resp = session.get(m3u_url, headers=headers)
+					resp.raise_for_status()
+					data = resp.text
+
+					if progress.wasCancelled():
+						raise KeyboardInterrupt
+
+				chunk_urls = [urljoin(m3u_url, line) for line in data.split('\n') if line and line[0] != '#']
+				chunk_count = len(chunk_urls)
+
+				progress.maximum = chunk_count
+				start_time = time()
+
+				with open(outfile,'wb') as fp:
+					for i, chunk_url in enumerate(chunk_urls):
+						print('downloading:',chunk_url)
+						with closing(session.get(chunk_url, headers=headers, stream=True)) as resp:
+							resp.raise_for_status()
+							for data in resp.iter_content(8192):
+								fp.write(data)
+								if progress.wasCancelled():
+									raise KeyboardInterrupt
+
+						value   = i + 1
+						elapsed = time() - start_time
+						avgtime = elapsed / value
+						esttime = avgtime * chunk_count
+						remtime = esttime - elapsed
+
+						progress.value = value
+						progress.setLabelText('Downloading »%s« ETA -%s' % (outname, fmt_span(remtime)))
+						if progress.wasCancelled():
+							raise KeyboardInterrupt
+
+		passive_popup('Filished saving video: '+outfile)
+
+	except KeyboardInterrupt:
+		print("\ndownload canceled by user")
+		if outfile is not None:
+			passive_popup('Download canceled by user: '+outfile)
 
 if __name__ == '__main__':
 	import sys
 	try:
 		get_video_from_m3u(*sys.argv[1:3])
 
-	except KeyboardInterrupt:
-		print("\ncanceled by user interrupt")
 	except Exception as e:
 		traceback.print_exc()
 		show_error(str(e))
